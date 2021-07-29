07ea75036ce634e8dc5941773c9a8c5be57a1f7d
swestfechtel <simon.westfechtel@gmail.com>
2021-07-29 11:18:55 +0200

    - change from os to pathlib - edits to paper

--- indexing.py
+++ indexing.py
@@ -1,4 +1,4 @@
-import os
+import pickle
 import json
 import logging
 import traceback
@@ -6,14 +6,14 @@ import traceback
 from pathlib import Path
 
 
-def scan_directory(directory, res_set):
+def scan_directory(directory: Path, res_set: set):
     logging.debug(f'Scanning {directory}.')
-    for subdir in os.scandir(directory):
+    for subdir in directory.iterdir():
         if subdir.is_dir():
             scan_directory(subdir, res_set)
         elif subdir.name == 'metadata.json':
             try:
-                f = open(n := Path(os.path.relpath(subdir)))
+                f = open(subdir)
                 j = json.load(f)
 
                 assert j['BodyPartExamined'] == 'KNEE'
@@ -22,11 +22,11 @@ def scan_directory(directory, res_set):
                 # assert j['ProtocolName'] == 'SAG 3D DESS WE'
                 # assert j['SeriesDescription'] == 'SAG3D'
 
-                res_set.add(p := Path(os.path.relpath(subdir)).parent)
+                res_set.add(p := subdir.parent)
                 logging.debug(f'Added {p} to result set.')
 
             except (FileNotFoundError, json.JSONDecodeError):
-                logging.debug(n)
+                logging.debug(subdir)
                 logging.debug(traceback.format_exc())
                 continue
             except (AssertionError, KeyError):
@@ -40,6 +40,11 @@ if __name__ == '__main__':
     logging.basicConfig(filename='/work/scratch/westfechtel/pylogs/indexing.log', encoding='utf-8',
                         level=logging.DEBUG, filemode='w')
     base_dir = '/images/Shape/Medical/Knees/OAI/FullNiftiTest'
+    base_dir = Path(base_dir)
     result_set = set()
     scan_directory(base_dir, result_set)
-    print(result_set)
+    with open('/work/scratch/westfechtel/pickles/index/index') as of:
+        pickle.dump(result_set, of)
+
+    for path in result_set:
+        print(path)
